679a5486c8f3a61601827ed4333e6a818b79953d
Eberti Falcao <ebertifalcao@gmail.com>
2020-05-24 04:17:06 -0300

    Criamos a Classe Usuario

--- index.php
+++ index.php
@@ -2,9 +2,17 @@
 
 require_once("config.php");
 
+$eberti = new Usuario();
+
+$eberti->loadById(1);
+
+echo $eberti;
+
+/*
 $sql = new Sql();
 
 $usuarios = $sql->select("select * from tb_usuarios");
 
 echo json_encode($usuarios);
+*/
 ?>
